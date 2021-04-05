a71c101d4712446caca97102cbaaf062e39eb263
Keikishim2 <67738474+Keikishim2@users.noreply.github.com>
2021-04-05 12:06:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require('simple_html_dom.php');
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/search?q=coding+ninjas');
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+//echo $result;
+
+$domResult = new simple_html_dom();
+$domResult->load($result);
+
+foreach($domResult->find('a[href^=/url?]') as $link)
+echo $link->plaintext . '<br>';
+
+?>
